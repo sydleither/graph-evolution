c9c1ecce93706893953fe7f6d97379512aee3357
Vincent Ragusa <vincent.ragusa.94@gmail.com>
2023-11-21 12:31:00 -0500

    converted to weighted digraph with negative weight

--- main.py
+++ main.py
@@ -5,18 +5,54 @@ from typing import Callable
 import matplotlib.pyplot as plt
 import networkx as nx
 
+# this function is based on:
+# Clifford Bohm, Arend Hintze, Jory Schossau; July 24–28, 2023.
+# "A Simple Sparsity Function to Promote Evolutionary Search." 
+# Proceedings of the ALIFE 2023: Ghost in the Machine: 
+# Proceedings of the 2023 Artificial Life Conference. 
+# ALIFE 2023: Ghost in the Machine: Proceedings of the 2023 Artificial Life Conference. 
+# Online. (pp. 53). ASME. https://doi.org/10.1162/isal_a_00655
+def sparsify(x,percentSparse:float = 0.5, outputRange:tuple[float]=(-1,1)):
+    assert 0 <= percentSparse <= 1
+    assert outputRange[0] <= 0 <= outputRange[1]
+    assert outputRange[0] != 0 or outputRange[1] != 0
+
+    percentNonZero = 1-percentSparse
+    neg = abs(outputRange[0])
+    pos = outputRange[1]
+    negPercent = neg/(pos+neg)
+    posPercent = pos/(pos+neg)
+    a = negPercent*percentNonZero
+    b = posPercent*percentNonZero
+    t1 = a
+    t2 = a + percentSparse
+
+    if x <= 0:
+        return -neg
+    if 0 < x <= t1:
+        return (neg/a)*x- neg
+    if t1 < x <= t2:
+        return 0
+    if t2 < x <= 1:
+        return (pos/b)*(x-t2)
+    if 1 < x:
+        return pos
+    
+
 
 class Organism:
-    def __init__(self, numNodes:int, sparsity:float=0.5) -> None:
-        #init numNodes x numNodes matrix with 'sparsity' percent of 1s (0.9 -> 90% 1s)
-        #TODO: support weighted digraphs using cliff/arend's sparsity function
-        self.adjacencyMatrix:list[list[int]] = [[1 if random() <= sparsity else 0 for _ in range(numNodes)] for _ in range(numNodes)]
+    def __init__(self, numNodes:int, sparsity:float=0.5, weightRange=(-1,1)) -> None:
+        #init numNodes x numNodes matrix with 'sparsity' percent of 0s, and weights in weightRange.
+        self.adjacencyMatrix:list[list[int]] = [[sparsify(random(),percentSparse=sparsity, outputRange=weightRange) 
+                                                 for _ in range(numNodes)] for _ in range(numNodes)]
         #internal size reference
         self.numNodes:int = numNodes
         #internal number of interactions reference
         self.numInteractions:int = sum([sum([1 for y in x if y != 0]) for x in self.adjacencyMatrix])
         #evaluation memo, for possible efficiency boosts (do not access directly, use getter)
         self.evaluationScores:list[float] = []
+        self.sparsity = sparsity
+        self.weightRange = weightRange
 
 
     def makeMutatedCopy(self, mutationRate:float = 0.005):
@@ -27,9 +63,7 @@ class Organism:
         for i in range(self.numNodes):
             for j in range(self.numNodes):
                 if random() <= mutationRate:
-                    #toggle bit (1-> 0, or 0->1)
-                    newOrg.adjacencyMatrix[i][j] *= -1
-                    newOrg.adjacencyMatrix[i][j] += 1
+                    newOrg.adjacencyMatrix[i][j] = sparsify(random(),percentSparse=self.sparsity, outputRange=self.weightRange) 
         return newOrg
     
 
@@ -45,10 +79,8 @@ class Organism:
         G = nx.DiGraph()
         for i in range(self.numNodes):
             for j in range(self.numNodes):
-                #TODO: support weighted digraphs using abs() >= 0
-                if self.adjacencyMatrix[i][j] == 1:
-                    #TODO: support weighted digraphs using weight=
-                    G.add_edge(i,j)
+                if abs(self.adjacencyMatrix[i][j]) > 0:
+                    G.add_edge(i,j,weight=self.adjacencyMatrix[i][j])
         return G
 
 
@@ -57,12 +89,12 @@ class Organism:
         ######################
         # grpah layout style #
         ######################
-        pos = nx.nx_agraph.graphviz_layout(G)
+        # pos = nx.nx_agraph.graphviz_layout(G)
         # pos = nx.kamada_kawai_layout(G)
         # pos = nx.spectral_layout(G)
         # pos = nx.planar_layout(G)
         # random
-        # pos = nx.shell_layout(G)
+        pos = nx.shell_layout(G)
         # pos = nx.spring_layout(G)
         ######################
 
@@ -70,10 +102,15 @@ class Organism:
         plt.title("Network Topology")
 
         nx.draw_networkx_nodes(G, pos=pos)
-        nx.draw_networkx_labels(G, pos, {node:node for node in G.nodes()}, font_size=9, font_color="k")
+        nx.draw_networkx_labels(G, pos,
+                                {node:node for node in G.nodes()},
+                                font_size=9,
+                                font_color="k")
 
-        nx.draw_networkx_edges(G, pos=pos) #TODO: support weighted digrpahs with weights
-        # nx.draw_networkx_edge_labels(G, pos=pos) #optional edge weight labels go here
+        nx.draw_networkx_edges(G, pos=pos)
+        nx.draw_networkx_edge_labels(G, pos=pos,
+                                     edge_labels={(n1,n2):round(data['weight'],3) for n1,n2,data in G.edges(data=True)},
+                                     label_pos=0.8)
 
         plt.savefig(path)
         plt.close()
@@ -129,11 +166,11 @@ def epsilonLexicase(population:list[Organism], numParents:int, epsilon:float = 0
 
 
 if __name__ == '__main__':
-    POPSIZE = 100
+    POPSIZE = 10
     MUTATION_RATE = 0.005
     NETWORK_SIZE = 10
-    NETWORK_SPARSITY = 0.5
-    NUM_GENERATIONS = 500
+    NETWORK_SPARSITY = 0.8
+    NUM_GENERATIONS = 10
     EVAL_FUNCS = [connectance,]
     IDEAL_VALS = [0.5,]
 
@@ -144,6 +181,8 @@ if __name__ == '__main__':
         parents = epsilonLexicase(population,POPSIZE)
         children = [parent.makeMutatedCopy(MUTATION_RATE) for parent in parents]
         population = children
+
+    population[0].saveGraphFigure("testFigure.png")
 
     for j in range(len(EVAL_FUNCS)):
         print()
