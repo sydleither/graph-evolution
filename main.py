4232510d0dc56f6ed2c5251ebb0b52bd60e0efdf
sydleither <sydleither@protonmail.com>
2023-12-12 13:25:18 -0500

    fix replication runs to work with hpcc or without

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 import json
+import os
+import pickle
 import sys
 
 import matplotlib.pyplot as plt
@@ -6,11 +8,13 @@ from bintools import numBins
 from eval_functions import Evaluation
 from ga import run
 
+
 #transpose a matrix (list of list)
 def T(LL:list[list])->list[list]:
     return list(zip(*LL))
 
-def final_pop_histogram(final_pop, eval_funcs, transparent=False):
+
+def final_pop_histogram(final_pop, eval_funcs, save_loc, transparent=False):
     eval = Evaluation()
     num_plots = len(eval_funcs)
     figure, axis = plt.subplots(1, num_plots, figsize=(4*num_plots,5)) #TODO: dynamically add new rows when columns are full
@@ -24,11 +28,11 @@ def final_pop_histogram(final_pop, eval_funcs, transparent=False):
         i += 1
     figure.tight_layout(rect=[0, 0.03, 1, 0.95])
     figure.suptitle('Final Population Histograms')
-    plt.savefig("histograms.png", transparent=transparent)
+    plt.savefig("{}/histograms.png".format(save_loc), transparent=transparent)
     plt.close()
 
 
-def plot_fitness(fitness_log, eval_func_names, transparent=False):
+def plot_fitness(fitness_log, eval_func_names, save_loc, transparent=False):
     figure, axis = plt.subplots(1, 1)
     for func_name in eval_func_names:
         axis.plot(fitness_log[func_name], label=func_name)
@@ -38,36 +42,59 @@ def plot_fitness(fitness_log, eval_func_names, transparent=False):
     figure.legend()
     if transparent:
         figure.patch.set_alpha(0.0)
-    plt.savefig("fitness.png")
+    plt.savefig("{}/fitness.png".format(save_loc))
     plt.close()
 
 
-def plotParetoFront(population,config,save=True):
+def plotParetoFront(population, config, save_loc=None):
     paretoFront = []
     for i in range(config["popsize"]):
         if not any([population[j] > population[i] for j in range(config["popsize"]) if j != i]):
             paretoFront.append(population[i])
     funcNames = list(config["eval_funcs"].keys())
-    for feature1,feature2 in zip(funcNames[:-1],funcNames[1:]):
-        R = sorted(sorted([(org.evaluationScores[feature1],org.evaluationScores[feature2]) for org in paretoFront],key=lambda r: r[1],reverse=True),key=lambda r: r[0])
-        plt.plot(*T(R),marker="o",linestyle="--")
+    for feature1, feature2 in zip(funcNames[:-1], funcNames[1:]):
+        R = sorted(sorted([(org.evaluationScores[feature1], org.evaluationScores[feature2]) for org in paretoFront], key=lambda r: r[1], reverse=True), key=lambda r: r[0])
+        plt.plot(*T(R), marker="o", linestyle="--")
         plt.title(feature1+" "+feature2)
         plt.xlabel(feature1 + " MSE")
         plt.ylabel(feature2 + " MSE")
-        if save:
-            plt.savefig("./pareto_{}_{}.png".format(feature1,feature2))
+        if save_loc:
+            plt.savefig("{}/pareto_{}_{}.png".format(save_loc, feature1, feature2))
             plt.close()
         else:
             plt.show()
 
-def main(config):
-    final_pop, fitness_log = run(config)
-    
-    plot_fitness(fitness_log, config["eval_funcs"].keys())
-    final_pop_histogram(final_pop, config["eval_funcs"])
-    final_pop[0].saveGraphFigure("./graphFigure.png")
-    plotParetoFront(final_pop,config,save=True)
 
+def run_rep(i, config):
+    save_loc = "data/{}/{}".format(config["name"], i)
+    os.makedirs(save_loc)
+
+    final_pop, fitness_log = run(config)
+
+    if config["save_data"] == 1:
+        with open("{}/final_pop.pkl".format(save_loc), "wb") as f:
+            pickle.dump(final_pop, f)
+        with open("{}/fitness_log.pkl".format(save_loc), "wb") as f:
+            pickle.dump(fitness_log, f)
+
+    if config["plot_data"] == 1:
+        plot_fitness(fitness_log, config["eval_funcs"].keys(), save_loc)
+        final_pop_histogram(final_pop, config["eval_funcs"], save_loc)
+        plotParetoFront(final_pop, config, save_loc)
+        final_pop[0].saveGraphFigure("{}/graphFigure.png".format(save_loc))
+
+
+def main(config, rep=None):
+    if rep:
+        run_rep(rep, config)
+    else:
+        for i in range(config["reps"]):
+            run_rep(i, config)
+
+    config_path = "data/{}/config.json".format(config["name"])
+    if not os.path.exists(config_path):
+        with open(config_path, "w") as f:
+            json.dump(config, f)
 
 
 if __name__ == "__main__":
@@ -77,4 +104,11 @@ if __name__ == "__main__":
     except:
         print("Please give a valid config json to read parameters from.")
         exit()
-    main(config)
+    
+    if len(sys.argv) == 2:
+        main(config)
+    elif len(sys.argv) == 3:
+        rep = sys.argv[2]
+        main(config, rep)
+    else:
+        print("Please pass in valid arguments: config and rep")
