b088a9d73d7e9fb7d0078d3e08c83cd1d18af419
sydleither <sydleither@protonmail.com>
2023-12-21 14:02:51 -0500

    start implementing systematic way to test combinations of objectives

---
+++ experiments/generate_configs.py
@@ -0,0 +1,71 @@
+import json
+import sys
+
+
+def connectance_configs(eval_funcs, property_names):
+    exp_names = []
+    for connectance in [0.25, 0.5, 0.75]:
+        for size in [10, 50, 100]:
+            exp_name = "connectance_{}_{}_{}".format(property_names, str(connectance).replace('.', ''), size)
+
+            eval_funcs["connectance"] = {"target":connectance}
+
+            config = {
+                "data_dir": "data",
+                "name": exp_name,
+                "reps": 1,
+                "save_data": 1,
+                "plot_data": 0,
+                "popsize": 100,
+                "mutation_rate": 0.005,
+                "mutation_odds": [1,2,1],
+                "crossover_odds": [1,2,2],
+                "crossover_rate": 0.6,
+                "weight_range": [0,1],
+                "network_size": size,
+                "network_sparsity": 1-connectance,
+                "num_generations": 500,
+                "epsilon": 0.025,
+                "eval_funcs": eval_funcs
+            }
+
+            config_path = "experiments/configs/{}.json".format(exp_name)
+            with open(config_path, "w") as f:
+                json.dump(config, f, indent=4)
+
+            exp_names.append(exp_name)
+    return exp_names
+
+
+def connectance_experiment(generate_script):
+    #generate config files
+    configs = []
+    configs += connectance_configs({}, "")
+
+    eval_funcs = {
+        "in_degree_distribution": {"name": "scale-free", "gamma": 1.5, "offset": 2},
+        "out_degree_distribution": {"name": "scale-free", "gamma": 1.5, "offset": 2},
+    }
+    configs += connectance_configs(eval_funcs, "DD")
+
+    #generate bash script to run all the configs on the hpcc
+    if generate_script:
+        with open("experiments/run_experiments_hpcc", "w") as f:
+            f.write("cd /mnt/scratch/leithers/graph-evolution\n")
+            f.write("cp /mnt/home/leithers/graph_evolution/graph-evolution/main.py .\n"+
+                    "cp /mnt/home/leithers/graph_evolution/graph-evolution/ga.py .\n"+
+                    "cp /mnt/home/leithers/graph_evolution/graph-evolution/eval_functions.py .\n"+
+                    "cp /mnt/home/leithers/graph_evolution/graph-evolution/organism.py .\n"+
+                    "cp /mnt/home/leithers/graph_evolution/graph-evolution/bintools.py .\n"+
+                    "cp /mnt/home/leithers/graph_evolution/graph-evolution/plot_utils.py .\n")
+            for config_name in configs:
+                f.write("sbatch /mnt/home/leithers/graph_evolution/graph-evolution/experiments/hpcc.sb {}.json\n".format(config_name))
+
+
+if __name__ == "__main__":
+    experiment_name = sys.argv[1]
+    generate_script = True if len(sys.argv) == 3 else False
+    if experiment_name == "connectance":
+        connectance_experiment(generate_script)
+    else:
+        print("Please give a valid experiment name.")
