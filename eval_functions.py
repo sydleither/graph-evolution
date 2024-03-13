9b4eb0211fc89140473cc18a425f7145595fa065
Vincent Ragusa <vincent.ragusa.94@gmail.com>
2024-03-13 14:37:28 -0400

    removed decorations from eval_funcs to avoid circular imports with Organism

--- eval_functions.py
+++ eval_functions.py
@@ -1,12 +1,10 @@
 import networkx as nx
 import numpy as np
 
-from organism import Organism
-
 
 class Evaluation:
     #node-level topological properties
-    def in_degree_distribution(self, network:Organism) -> list[float]:
+    def in_degree_distribution(self, network) -> list[float]:
         networkx_obj = network.getNetworkxObject()
         num_nodes = network.numNodes
         degree_sequence = list(d for _, d in networkx_obj.in_degree())
@@ -16,7 +14,7 @@ class Evaluation:
         return freq
     
 
-    def out_degree_distribution(self, network:Organism) -> list[float]:
+    def out_degree_distribution(self, network) -> list[float]:
         networkx_obj = network.getNetworkxObject()
         num_nodes = network.numNodes
         degree_sequence = list(d for _, d in networkx_obj.out_degree())
@@ -26,49 +24,49 @@ class Evaluation:
         return freq
 
 
-    def avg_shortest_path_length_distribution(self, network:Organism) -> list[float]:
+    def avg_shortest_path_length_distribution(self, network) -> list[float]:
         weight = 1/network.numNodes**2
         shortest_path = dict(nx.shortest_path_length(network.getNetworkxObject()))
         avg_shortest = sorted([weight*np.mean(list(shortest_path[i].values())) for i in range(len(shortest_path))], reverse=True)
         return avg_shortest
     
 
-    def betweenness_distribution(self, network:Organism) -> list[float]:
+    def betweenness_distribution(self, network) -> list[float]:
         networkx_obj = network.getNetworkxObject()
         return sorted(list(nx.betweenness_centrality(networkx_obj).values()), reverse=True)
     
     
     #topological properties
-    def strong_components(self, network:Organism) -> int:
+    def strong_components(self, network) -> int:
         return len(list(nx.strongly_connected_components(network.getNetworkxObject())))
 
 
-    def weak_components(self, network:Organism) -> int:
+    def weak_components(self, network) -> int:
         return len(list(nx.weakly_connected_components(network.getNetworkxObject())))
 
 
-    def connectance(self, network:Organism) -> float:
+    def connectance(self, network) -> float:
         return network.numInteractions / network.numNodes**2
 
 
-    def clustering_coefficient(self, network:Organism) -> float:
+    def clustering_coefficient(self, network) -> float:
         return nx.average_clustering(network.getNetworkxObject())
     
 
-    def transitivity(self, network:Organism) -> float:
+    def transitivity(self, network) -> float:
         return nx.transitivity(network.getNetworkxObject())
 
 
-    def proportion_of_self_loops(self, network:Organism) -> float:
+    def proportion_of_self_loops(self, network) -> float:
         return sum([1 for i in range(network.numNodes) if network.adjacencyMatrix[i][i] != 0]) / network.numNodes
     
 
-    def diameter(self, network:Organism) -> int:
+    def diameter(self, network) -> int:
         shortest_path = dict(nx.shortest_path_length(network.getNetworkxObject()))
         return max([max(shortest_path[i].values()) for i in range(len(shortest_path))])
     
 
-    def number_of_modules(self, network:Organism) -> int:
+    def number_of_modules(self, network) -> int:
         if network.numInteractions > 0:
             return len(nx.community.greedy_modularity_communities(network.getNetworkxObject()))
         else:
@@ -76,7 +74,7 @@ class Evaluation:
     
 
     #node-level interaction strength properties
-    def pos_out_weight_distribution(self, network:Organism) -> float:
+    def pos_out_weight_distribution(self, network) -> float:
         networkx_obj = network.getNetworkxObject()
         matrix = network.adjacencyMatrix
         num_nodes = network.numNodes
@@ -90,7 +88,7 @@ class Evaluation:
         return avg_weights
 
 
-    def pos_in_weight_distribution(self, network:Organism) -> float:
+    def pos_in_weight_distribution(self, network) -> float:
         networkx_obj = network.getNetworkxObject()
         matrix_T = list(zip(*network.adjacencyMatrix))
         num_nodes = network.numNodes
@@ -104,7 +102,7 @@ class Evaluation:
         return avg_weights
 
 
-    def neg_out_weight_distribution(self, network:Organism) -> float:
+    def neg_out_weight_distribution(self, network) -> float:
         networkx_obj = network.getNetworkxObject()
         matrix = network.adjacencyMatrix
         num_nodes = network.numNodes
@@ -118,7 +116,7 @@ class Evaluation:
         return avg_weights
 
 
-    def neg_in_weight_distribution(self, network:Organism) -> float:
+    def neg_in_weight_distribution(self, network) -> float:
         networkx_obj = network.getNetworkxObject()
         matrix_T = list(zip(*network.adjacencyMatrix))
         num_nodes = network.numNodes
@@ -133,40 +131,40 @@ class Evaluation:
 
 
     #interaction strength properties
-    def positive_interactions_proportion(self, network:Organism) -> float:
+    def positive_interactions_proportion(self, network) -> float:
         num_interactions = network.numInteractions
         return network.numPositive / num_interactions if num_interactions > 0 else 0
 
 
-    def average_positive_interactions_strength(self, network:Organism) -> float:
+    def average_positive_interactions_strength(self, network) -> float:
         sum_pos = sum([sum([y for y in x if y > 0]) for x in network.adjacencyMatrix])
         num_pos = network.numPositive
         return  sum_pos / num_pos if num_pos > 0 else 0
     
 
-    def average_negative_interactions_strength(self, network:Organism) -> float:
+    def average_negative_interactions_strength(self, network) -> float:
         sum_neg = sum([sum([y for y in x if y < 0]) for x in network.adjacencyMatrix])
         num_neg = network.numNegative
         return  sum_neg / num_neg if num_neg > 0 else 0
 
 
-    def proportion_of_self_loops_positive(self, network:Organism) -> float:
+    def proportion_of_self_loops_positive(self, network) -> float:
         return sum([1 for i in range(network.numNodes) if network.adjacencyMatrix[i][i] > 0]) / network.numNodes
 
 
-    def number_of_mutualistic_pairs(self, network:Organism) -> int:
+    def number_of_mutualistic_pairs(self, network) -> int:
         adj = network.adjacencyMatrix
         nn = network.numNodes
         return sum([sum([1 for j in range(i+1, nn) if adj[i][j] > 0 and adj[j][i] > 0]) for i in range(nn)])
 
 
-    def number_of_competiton_pairs(self, network:Organism) -> int:
+    def number_of_competiton_pairs(self, network) -> int:
         adj = network.adjacencyMatrix
         nn = network.numNodes
         return sum([sum([1 for j in range(i+1, nn) if adj[i][j] < 0 and adj[j][i] < 0]) for i in range(nn)])
 
 
-    def number_of_parasitism_pairs(self, network:Organism) -> int:
+    def number_of_parasitism_pairs(self, network) -> int:
         adj = network.adjacencyMatrix
         nn = network.numNodes
         return sum([sum([1 for j in range(i+1, nn) if (adj[i][j] < 0 and adj[j][i] > 0) or (adj[i][j] > 0 and adj[j][i] < 0)]) for i in range(nn)])
