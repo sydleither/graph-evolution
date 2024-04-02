87ece0a21d54af3b77062060778004b22f276668
sydleither <sydleither@protonmail.com>
2024-04-02 17:43:17 -0400

    replicate map elites plots

--- main.py
+++ main.py
@@ -1,8 +1,6 @@
-from itertools import combinations
 import json
 import os
 import pickle
-from statistics import mean
 import sys
 from collections import Counter
 
@@ -13,7 +11,7 @@ from elites import run as elitesrun
 from eval_functions import functions
 from nsga import fast_non_dominated_sort, run as nsgarun
 from organism import Organism
-from plot_utils import T, final_pop_distribution, final_pop_histogram
+from plot_utils import T, final_pop_distribution, final_pop_histogram, plot_elites_map
 from random import seed
 
 
@@ -23,7 +21,7 @@ def plot_fitness(fitness_log, eval_func_names, save_loc, transparent=False):
         axis.plot(fitness_log[func_name], label=func_name)
     axis.set_yscale("log")
     figure.supxlabel("Generations")
-    figure.supylabel("MSE")
+    figure.supylabel("Error")
     figure.legend()
     if transparent:
         figure.patch.set_alpha(0.0)
@@ -36,62 +34,11 @@ def plot_coverage(coverage, save_loc, transparent=False):
     axis.plot(coverage)
     figure.supxlabel("Generations")
     figure.supylabel("Coverage")
+    figure.tight_layout()
     if transparent:
         figure.patch.set_alpha(0.0)
     plt.savefig("{}/coverage.png".format(save_loc))
     plt.close()
-
-
-def plot_elites_map(elites_map, eval_funcs, features_dict, save_loc, transparent=False):
-    def generate_heatmap(col, row, obj_name, obj_target):
-        mean_heatmap = np.empty([len(row), len(col)])
-        count_heatmap = np.empty([len(row), len(col)])
-        for i in range(len(col)):
-            for j in range(len(row)):
-                cell = elites_map[(col[i], row[j])] if len(row) > 1 else elites_map[(col[i],)]
-                if len(cell) > 0:
-                    mean_heatmap[j,i] = round(mean([org.getError(obj_name, obj_target) for org in cell]), 3)
-                else:
-                    mean_heatmap[j,i] = 9999
-                count_heatmap[j,i] = len(cell)
-        return mean_heatmap, count_heatmap
-    
-    def save_heatmaps(mean_heatmap, count_heatmap, col_name, row_name, objective):
-        col_labels = features_dict[col_name]
-        row_labels = features_dict[row_name] if row_name is not None else [""]
-        figure, axis = plt.subplots(1, 2, figsize=(12,7))
-        axis[0].imshow(mean_heatmap, cmap="Greens")
-        axis[0].set_xticks(np.arange(len(col_labels)), labels=col_labels)
-        axis[0].set_yticks(np.arange(len(row_labels)), labels=row_labels)
-        axis[0].set_title("Mean Cell {} Error".format(objective))
-        axis[1].imshow(count_heatmap, cmap="Greens_r")
-        axis[1].set_xticks(np.arange(len(col_labels)), labels=col_labels)
-        axis[1].set_yticks(np.arange(len(row_labels)), labels=row_labels)
-        axis[1].set_title("Count of Organisms in Each Cell")
-        for i in range(len(col_labels)):
-            for j in range(len(row_labels)):
-                _ = axis[0].text(i, j, mean_heatmap[j, i], ha="center", va="center", color="black")
-                _ = axis[1].text(i, j, count_heatmap[j, i], ha="center", va="center", color="black")
-        figure.supxlabel(col_name)
-        figure.supylabel(row_name)
-        figure.tight_layout()
-        if transparent:
-            figure.patch.set_alpha(0.0)
-        plt.savefig("{}/heatmap_{}.png".format(save_loc, objective))
-        plt.close()
-
-    feature_names = list(features_dict.keys())
-    feature_bins = list(features_dict.values())
-    for name,target in eval_funcs.items():
-        if len(features_dict) == 1:
-            mean_heatmap, count_heatmap = generate_heatmap(feature_bins[0], [None], name, target)
-            save_heatmaps(mean_heatmap, count_heatmap, feature_names[0], None, name)
-        elif len(features_dict) == 2:
-            mean_heatmap, count_heatmap = generate_heatmap(feature_bins[0], feature_bins[1], name, target)
-            save_heatmaps(mean_heatmap, count_heatmap, feature_names[0], feature_names[1], name)
-        else:
-            print("Too many features to plot elites map.")
-            return
 
 
 def plotParetoFront(population, config, save_loc=None,firstFrontOnly=False):
@@ -108,8 +55,8 @@ def plotParetoFront(population, config, save_loc=None,firstFrontOnly=False):
                 plt.plot(*T(R), marker="o", linestyle="--",label=frontNumber)
                 if firstFrontOnly: break
             plt.title(feature1+" "+feature2)
-            plt.xlabel(feature1 + " MSE")
-            plt.ylabel(feature2 + " MSE")
+            plt.xlabel(feature1 + " Error")
+            plt.ylabel(feature2 + " Error")
             plt.legend()
             if save_loc is not None:
                 plt.savefig("{}/pareto_{}_{}.png".format(save_loc, feature1, feature2))
--- replicate_analysis.py
+++ replicate_analysis.py
@@ -9,9 +9,10 @@ import matplotlib
 import matplotlib.pyplot as plt
 import numpy as np
 
-from ga import fast_non_dominated_sort
+from nsga import fast_non_dominated_sort
 from plot_utils import (T, calculate_confidence_interval,
-                        final_pop_distribution, final_pop_histogram)
+                        final_pop_distribution, final_pop_histogram,
+                        plot_elites_map)
 
 
 def plot_fitnesses_error(fitness_logs, eval_func_names, save_loc, transparent=False):
@@ -37,23 +38,40 @@ def plot_fitnesses_sep(fitness_logs, eval_func_names, save_loc, transparent=Fals
     num_plots = len(eval_func_names)
     num_plots = num_plots if num_plots > 1 else 2
     figure, axis = plt.subplots(num_plots, 1, figsize=(5,3*num_plots))
-    i = 0
+    sp = 0
     for func_name in eval_func_names:
         for run in range(len(fitness_logs)):
-            axis[i].plot(fitness_logs[run][func_name], alpha=0.5, color='gray')
+            axis[sp].plot(fitness_logs[run][func_name], alpha=0.5, color='gray')
         eval_func_data = [fitness_logs[i][func_name] for i in range(len(fitness_logs))]
         eval_func_data_mean = [np.mean([eval_func_data[i][j] for i in range(len(eval_func_data))]) for j in range(len(eval_func_data[0]))]
-        axis[i].plot(eval_func_data_mean, color="forestgreen")
-        axis[i].set_title(func_name)
-        axis[i].set_yscale("log")
-        i += 1
+        axis[sp].plot(eval_func_data_mean, color="forestgreen")
+        axis[sp].set_title(func_name)
+        axis[sp].set_yscale("log")
+        sp += 1
     figure.supxlabel("Generations")
-    figure.supylabel("MSE")
+    figure.supylabel("Error")
     figure.suptitle("Error Over Time")
     figure.tight_layout(rect=[0, 0.03, 1, 0.95])
     if transparent:
         figure.patch.set_alpha(0.0)
     plt.savefig("{}/fitness.png".format(save_loc))
+    plt.close()
+
+
+def plot_coverage(coverages, save_loc, transparent=False):
+    figure, axis = plt.subplots(2, 1)
+    coverage_mean, neg_error, pos_error = calculate_confidence_interval(coverages)
+    for run in range(len(coverages)):
+        axis[0].plot(coverages[run], alpha=0.5, color='gray')
+    axis[0].plot(coverage_mean, color="forestgreen")
+    axis[1].plot(coverage_mean, color="forestgreen")
+    axis[1].fill_between(range(len(coverage_mean)), neg_error, pos_error, alpha=0.5, color="forestgreen")
+    figure.supxlabel("Generations")
+    figure.supylabel("Coverage")
+    figure.tight_layout()
+    if transparent:
+        figure.patch.set_alpha(0.0)
+    plt.savefig("{}/coverage.png".format(save_loc))
     plt.close()
 
 
@@ -76,8 +94,8 @@ def combined_pareto_front(final_pops, config, save_loc=None, firstFrontOnly=Fals
                 plt.plot(*T(R), marker="o", linestyle="--",label=frontNumber)
                 if firstFrontOnly: break
             plt.title(feature1+" "+feature2)
-            plt.xlabel(feature1 + " MSE")
-            plt.ylabel(feature2 + " MSE")
+            plt.xlabel(feature1 + " Error")
+            plt.ylabel(feature2 + " Error")
             plt.legend()
             if save_loc is not None:
                 plt.savefig("{}/pareto_{}_{}.png".format(save_loc, feature1, feature2))
@@ -106,6 +124,8 @@ def main(config_dir):
     final_pops = []
     fitness_logs = []
     entropy_logs = []
+    elites_maps = []
+    coverages = []
 
     for run_dir in os.listdir(config_dir):
         full_path = "{}/{}".format(config_dir, run_dir)
@@ -117,6 +137,12 @@ def main(config_dir):
                     final_pops.append(pickle.load(f))
                 with open("{}/fitness_log.pkl".format(full_path), "rb") as f:
                     fitness_logs.append(pickle.load(f))
+            if os.path.exists("{}/elites_map.pkl".format(full_path)):
+                with open("{}/elites_map.pkl".format(full_path), "rb") as f:
+                    elites_maps.append(pickle.load(f))
+                with open("{}/coverage.pkl".format(full_path), "rb") as f:
+                    coverages.append(pickle.load(f))
+            else: #TODO this is temp until diversity measures are fixed
                 with open("{}/diversity.csv".format(full_path), "r") as f:
                     rdr = reader(f)
                     _ = next(f) #remove header
@@ -127,12 +153,21 @@ def main(config_dir):
         os.makedirs(data_path)
 
     eval_funcs = config_file["eval_funcs"]
-    # final_pop_histogram(final_pops, eval_funcs, data_path, plot_all=True)
-    # final_pop_distribution(final_pops, eval_funcs, data_path, plot_all=True)
-    # plot_fitnesses_sep(fitness_logs, eval_funcs.keys(), data_path)
-    # plot_fitnesses_error(fitness_logs, eval_funcs.keys(), data_path)
-    # combined_pareto_front(final_pops, config_file, data_path)
-    combined_diversity(entropy_logs, data_path)
+    final_pop_histogram(final_pops, eval_funcs, data_path, plot_all=True)
+    final_pop_distribution(final_pops, eval_funcs, data_path, plot_all=True)
+    plot_fitnesses_sep(fitness_logs, eval_funcs.keys(), data_path)
+    plot_fitnesses_error(fitness_logs, eval_funcs.keys(), data_path)
+    combined_pareto_front(final_pops, config_file, data_path)
+    if "diversity_funcs" in config_file:
+        plot_coverage(coverages, data_path)
+        combo_elites_map = {}
+        map_keys = elites_maps[0].keys()
+        for map_key in map_keys:
+            all_cells = [elite_map[map_key] for elite_map in elites_maps]
+            combo_elites_map[map_key] = [org for cell in all_cells for org in cell]
+        plot_elites_map(combo_elites_map, config_file["eval_funcs"], config_file["diversity_funcs"], data_path)
+    else:
+        combined_diversity(entropy_logs, data_path)
 
 
 if __name__ == "__main__":
